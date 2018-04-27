30102cd9f88024c57d3cf6ccfa1d9eee8788ed41
Mikhail Pogretskiy <mpogret@mera.ru>
2018-04-27 12:44:24 +0300

    Update init script to ignore local files

--- init.sh
+++ init.sh
@@ -6,7 +6,9 @@
 DOTFILES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 cd $DOTFILES_DIR && git pull
-echo "*/local/*" >> .git/info/exclude # TODO: Update to write it only once
+sed -i '/*\/local\/*/d' ./.git/info/exclude
+echo "*/local/*" >> .git/info/exclude
+git update-index --skip-worktree .zshrclocal.sh
 
 ln -s $DOTFILES_DIR/.zshrc $HOME/.zshrc
 if [[ -e $DOTFILES_DIR/.zshrclocal.sh ]]; then ln -s $DOTFILES_DIR/.zshrclocal.sh $HOME/.zshrclocal.sh; fi
