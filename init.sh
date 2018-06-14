3fe2a166164117f74289208fe865f7f15fb53f60
Anfid <a4343z@rambler.ru>
2018-06-15 01:05:36 +0300

    Add reminder to init.sh

--- init.sh
+++ init.sh
@@ -23,13 +23,18 @@ git clone https://github.com/VundleVim/Vundle.vim.git ~/.config/nvim/bundle/Vund
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install
 
+sudo apt-get update
 sudo apt-get install -y silversearcher-ag
 
 sudo apt-get install -y software-properties-common python-dev python-pip python3-dev python3-pip
+
+#neovim
 sudo add-apt-repository -y ppa:neovim-ppa/stable
-sudo apt-get update
 sudo apt-get install -y neovim
 sudo apt-get install -y python-neovim python3-neovim
+
+# i3
+sudo apt-get install -y i3 i3status
 echo "Yes to all to use neovim instead of vim"
 sudo update-alternatives --install /usr/bin/vi vi /usr/bin/nvim 60
 sudo update-alternatives --config vi
@@ -37,3 +42,14 @@ sudo update-alternatives --install /usr/bin/vim vim /usr/bin/nvim 60
 sudo update-alternatives --config vim
 sudo update-alternatives --install /usr/bin/editor editor /usr/bin/nvim 60
 sudo update-alternatives --config editor
+
+# Reminders. Possibly add to script later
+
+# install rust via rustup. See manual for details.
+
+# Install ripgrep via snap or cargo.
+
+# Install FontAwesome for nice icons on i3 bar:
+# Link: https://github.com/FortAwesome/Font-Awesome/releases
+# Cheatsheet: https://fontawesome.com/cheatsheet?from=io
+# And ofc Input font for neovim
