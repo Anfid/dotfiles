63013b6b3126570ee7c7de20d2df9f78a232db8c
Mikhail Pogretskiy <mpogret@mera.ru>
2018-06-27 20:42:08 +0300

    Fixes and small improvements

--- .scripts/start-pb.sh
+++ .scripts/start-pb.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
 source $HOME/.cache/wal/colors-pb.sh
+export WLAN_IFACE=`iw dev | awk '$1=="Interface"{print $2}'`
+export ETH_IFACE=`ip link | awk -F': ' '$0 !~ "lo|vir|wl|^[^0-9]" && $3 ~ "state UP" {print $2}'`
 
 pkill polybar
 sleep 1
 if type "xrandr"; then
   for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-    MONITOR=$m polybar bottom &
+    MONITOR=$m polybar -r bottom &
   done
 else
-  polybar bottom &
+  polybar -r bottom &
 fi
