1970026b276411e7e2e068cef8959e1d3a6e7eca
Mikhail Pogretskiy <mpogret@mera.ru>
2018-09-26 21:54:34 +0300

    Polybar: Improve starting script

--- .scripts/start-pb.sh
+++ .scripts/start-pb.sh
@@ -3,13 +3,11 @@
 export WLAN_IFACE=`iw dev | awk '$1=="Interface"{print $2}'`
 export ETH_IFACE=`ip link | awk -F': ' '$0 !~ "lo|vir|wl|^[^0-9]" && $3 ~ "state UP" {print $2}'`
 
-killall polybar
-sleep 1
+killall -q polybar
+while pgrep -x polybar >/dev/null; do sleep 0.3; done
+
 source $HOME/.cache/wal/colors-alpha.sh
-if type "xrandr"; then
-  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-    MONITOR=$m polybar -r bottom &
-  done
-else
-  polybar -r bottom &
-fi
+MONITORS=$(polybar -m | awk -F: '{print $1}')
+for m in $MONITORS; do
+  MONITOR=$m polybar -r bottom &
+done
