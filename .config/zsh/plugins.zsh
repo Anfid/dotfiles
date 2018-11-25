97c9e3a0b43a9e8020109ec89c143c7a02fb0c18
Anfid <pogretzkiy.mihail@gmail.com>
2018-11-25 16:44:41 +0300

    Zsh: Add alias-tips plugin, tune autosuggetions

--- .config/zsh/plugins.zsh
+++ .config/zsh/plugins.zsh
@@ -1,16 +1,23 @@
 # Self-management
 zplug 'zplug/zplug', hook-build:'zplug --self-manage'
 
-# Git utils
+# Provides utilities, comptetions and aliases
 zplug "plugins/git",   from:oh-my-zsh
+zplug "plugins/pip",   from:oh-my-zsh
+zplug "plugins/cargo", from:oh-my-zsh
 
-# Completions
-zplug "plugins/pip",   lazy:true, from:oh-my-zsh
-zplug "plugins/cargo", lazy:true, from:oh-my-zsh
-zplug "zpm-zsh/ssh",   lazy:true
+# Reminds available aliases
+zplug "djui/alias-tips"
 
 # Fish-like suggestions as-you-type
-zplug "zsh-users/zsh-autosuggestions"
+function zsh-autosuggestions-override() {
+    # ignore commands in vi-mode
+    ZSH_AUTOSUGGEST_ACCEPT_WIDGETS=(forward-char end-of-line)
+    ZSH_AUTOSUGGEST_USE_ASYNC=true
+    ZSH_AUTOSUGGEST_STRATEGY=match_prev_cmd
+    ZSH_AUTOSUGGEST_CLEAR_WIDGETS+=vi-cmd-mode
+}
+zplug "zsh-users/zsh-autosuggestions", hook-load:zsh-autosuggestions-override
 
 # vi-mode plugin
 MODE_CURSOR_VICMD="blinking block"
@@ -19,14 +26,14 @@ zplug "softmoth/zsh-vim-mode"
 # viexchange messes up with syntax-highlighting. Make sure it loads after highlighting
 zplug "okapia/zsh-viexchange", on:"softmoth/zsh-vim-mode", defer:3
 
+# Substring history search
+# Load after vim-mode to prevent vim bindings to perform substring search
+zplug "zsh-users/zsh-history-substring-search", defer:1
+
 # Close matching paired characters
 zplug "hlissner/zsh-autopair", defer:2
 
-# Substring history search
-# Load after vim-mode to prevent vim bindings to substring search
-zplug "zsh-users/zsh-history-substring-search", lazy:true, defer:1
-
-# syntax highlight
+# Syntax highlight
 zplug "zdharma/fast-syntax-highlighting", defer:2
 
 # Install plugins if there are plugins that have not been installed
