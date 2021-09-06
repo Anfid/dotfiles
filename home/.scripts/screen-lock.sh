7a3169a6f6fb76e78a9893d8ad06fa00955beb5c
Mikhail Pogretskiy <mikhail.pogretskiy@gmail.com>
2021-09-07 01:22:18 +0300

    i3lock: Fix updated flag spelling

--- home/.scripts/screen-lock.sh
+++ home/.scripts/screen-lock.sh
@@ -3,11 +3,11 @@
 export LC_ALL=C
 source $HOME/.cache/wal/colors.sh
 i3lock --blur 7 --radius 250 --indicator \
-       --insidecolor="$color3"40 --insidevercolor="$color6"40   --insidewrongcolor=ff555540 \
-       --ringcolor="$background"80 --ringvercolor="$background"80 --ringwrongcolor=ff555580 \
-       --linecolor="$color6"80 --separatorcolor="$background"80 \
-       --keyhlcolor="$color6"c0 --bshlcolor=ff5555ff \
-       --verifcolor="$background"ff wrongcolor="$background"ff --timecolor="$background"ff --datecolor="$background"ff \
-       --veriftext="Unlocking..." --wrongtext="Wrong password" \
-       --clock --timestr="%I:%M%p" --time-font="Santana" --timesize=64 --timepos ix:iy \
-       --datestr="%A. %d %B %Y" --date-font="Santana" --datesize=32 --datepos tx:ty+48
+       --inside-color="$color3"40 --insidever-color="$color6"40   --insidewrong-color=ff555540 \
+       --ring-color="$background"80 --ringver-color="$background"80 --ringwrong-color=ff555580 \
+       --line-color="$color6"80 --separator-color="$background"80 \
+       --keyhl-color="$color6"c0 --bshl-color=ff5555ff \
+       --verif-color="$background"ff wrong-color="$background"ff --time-color="$background"ff --date-color="$background"ff \
+       --verif-text="Unlocking..." --wrong-text="Wrong password" \
+       --clock --time-str="%I:%M%p" --time-font="Santana" --time-size=64 --time-pos ix:iy \
+       --date-str="%A. %d %B %Y" --date-font="Santana" --date-size=32 --date-pos tx:ty+48
