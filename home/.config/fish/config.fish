04fdfcb3b93c1b02bd9bcec256536b70a3096d6b
Mikhail Pogretskiy <mikhail.pogretskiy@gmail.com>
2025-01-20 00:01:38 +0400

    Fish: Remove useless prompt items

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -8,6 +8,8 @@ set --global fish_greeting ""
 
 # Tide prompt
 set --global tide_left_prompt_items pwd git
-set --global tide_right_prompt_items status cmd_duration context jobs node python rustc
+set --global tide_right_prompt_items status cmd_duration context jobs
 
 set fzf_preview_dir_cmd eza --all --color=always
+
+set --global BAT_THEME base16
